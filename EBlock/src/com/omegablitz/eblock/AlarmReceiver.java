d3327ebdd6973eca45f03c9584b120cba60089a9
bobacadodl <ankursundara@msn.com>
2014-11-04 00:30:38 -0500

    derp fixes

--- EBlock/src/com/omegablitz/eblock/AlarmReceiver.java
+++ EBlock/src/com/omegablitz/eblock/AlarmReceiver.java
@@ -3,6 +3,7 @@ package com.omegablitz.eblock;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 /**
  * User: bobacadodl
@@ -12,10 +13,13 @@ import android.content.Intent;
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.notification_icon)
-                        .setContentTitle("My notification")
-                        .setContentText("Hello World!");
+        SharedPreferences settings = context.getSharedPreferences(MainActivity.SAVED_ID, 0);
+        if (settings.contains("id")) {
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.drawable.notification_icon)
+                            .setContentTitle("My notification")
+                            .setContentText("Hello World!");
+        }
     }
 }
--- EBlock/src/com/omegablitz/eblock/BootReceiver.java
+++ EBlock/src/com/omegablitz/eblock/BootReceiver.java
@@ -1,8 +1,12 @@
 package com.omegablitz.eblock;
 
+import android.app.AlarmManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import java.util.Calendar;
 
 /**
  * User: bobacadodl
@@ -15,7 +19,15 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-            MainActivity.setupAlarm(context);
+            // Set the alarm to start at 10:00 a.m.
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(System.currentTimeMillis());
+            calendar.set(Calendar.HOUR_OF_DAY, 10);
+            calendar.set(Calendar.MINUTE, 0);
+            alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 1000 * 60 * 60 * 24, alarmIntent);
+            pm.setComponentEnabledSetting(receiver,
+                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                    PackageManager.DONT_KILL_APP);
         }
     }
 }
